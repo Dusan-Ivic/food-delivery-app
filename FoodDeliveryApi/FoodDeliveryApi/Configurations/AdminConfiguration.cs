1b9a5b13b64b57de66234d563301f1ac67935d4a
Dusan-Ivic <ivicd99@gmail.com>
2023-07-24 14:53:15 +0200

    Add base user configuration file

--- FoodDeliveryApi/FoodDeliveryApi/Configurations/AdminConfiguration.cs
+++ FoodDeliveryApi/FoodDeliveryApi/Configurations/AdminConfiguration.cs
@@ -1,27 +1,13 @@
 ﻿using FoodDeliveryApi.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FoodDeliveryApi.Configurations
 {
-    public class AdminConfiguration : IEntityTypeConfiguration<User>
+    public class AdminConfiguration : UserConfiguration
     {
-        public void Configure(EntityTypeBuilder<User> builder)
+        public override void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).ValueGeneratedOnAdd();
-
-            builder.Property(x => x.Username).IsRequired().HasMaxLength(20);
-            builder.HasIndex(x => x.Username).IsUnique();
-
-            builder.Property(x => x.Password).IsRequired().HasMaxLength(60);
-
-            builder.Property(x => x.Email).IsRequired().HasMaxLength(254);
-            builder.HasIndex(x => x.Email).IsUnique();
-
-            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(50);
-
-            builder.Property(x => x.LastName).IsRequired().HasMaxLength(50);
+            base.Configure(builder);
         }
     }
 }
