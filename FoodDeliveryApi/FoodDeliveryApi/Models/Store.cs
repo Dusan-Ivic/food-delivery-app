e34ae16658be2b671f4fa376a8fcec133c23959e
Dusan-Ivic <ivicd99@gmail.com>
2023-07-25 16:56:59 +0200

    Create and configure Store model

---
+++ FoodDeliveryApi/FoodDeliveryApi/Models/Store.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodDeliveryApi.Models
+{
+    public class Store
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public long PartnerId { get; set; }
+        public Partner Partner { get; set; } = default!;
+    }
+}
