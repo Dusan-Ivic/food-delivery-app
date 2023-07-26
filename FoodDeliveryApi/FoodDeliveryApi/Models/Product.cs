e0bb4b92babdbd649fb166bd6847e81b1ee5c601
Dusan-Ivic <ivicd99@gmail.com>
2023-07-26 16:39:45 +0200

    Add product model and configuration

---
+++ FoodDeliveryApi/FoodDeliveryApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodDeliveryApi.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public long StoreId { get; set; }
+        public Store Store { get; set; } = default!;
+
+    }
+}
