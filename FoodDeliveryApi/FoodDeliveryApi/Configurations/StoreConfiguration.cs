7f39798c2c90ed2873addcce87acbb0a19dee459
Dusan-Ivic <ivicd99@gmail.com>
2023-08-02 01:08:27 +0200

    Set default values for ImageData columns

--- FoodDeliveryApi/FoodDeliveryApi/Configurations/StoreConfiguration.cs
+++ FoodDeliveryApi/FoodDeliveryApi/Configurations/StoreConfiguration.cs
@@ -30,7 +30,7 @@ namespace FoodDeliveryApi.Configurations
 
             builder.Property(x => x.Category).IsRequired().HasMaxLength(20);
 
-            builder.Property(x => x.ImageData).HasColumnType("bytea");
+            builder.Property(x => x.ImageData).HasColumnType("bytea").HasDefaultValue(Array.Empty<byte>());
         }
     }
 }
