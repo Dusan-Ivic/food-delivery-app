317f7dd70558867af0d60bc6438ad4e9865ec4c8
Dusan-Ivic <ivicd99@gmail.com>
2023-08-01 15:08:22 +0200

    Style order status field

--- food-delivery-client/src/components/OrderHistory.tsx
+++ food-delivery-client/src/components/OrderHistory.tsx
@@ -4,12 +4,15 @@ import { formatCurrency } from "../utils/currencyFormatter";
 import moment from "moment";
 import { useState } from "react";
 import { OrderModal } from "./OrderModal";
+import { GoDotFill } from "react-icons/go";
 
 interface OrderHistoryProps {
   orders: Order[];
   canManageOrders: boolean;
   onCancelOrder: (orderId: number) => void;
 }
+
+type OrderStatus = "Pending" | "Canceled" | "Completed";
 
 export function OrderHistory({
   orders,
@@ -37,18 +40,24 @@ export function OrderHistory({
     setModalOrder(null);
   };
 
-  const getOrderStatus = (order: Order) => {
+  const getOrderStatus = (order: Order): OrderStatus => {
     if (order.isCanceled) {
       return "Canceled";
     } else if (
       moment(order.createdAt)
-        .add(order.store.deliveryOptions.deliveryTimeInMinutes, "m")
+        .add(order.store?.deliveryOptions.deliveryTimeInMinutes, "m")
         .isAfter()
     ) {
       return "Pending";
     } else {
       return "Completed";
     }
+  };
+
+  const orderStatusToColorMap = {
+    Pending: "gold",
+    Canceled: "red",
+    Completed: "green",
   };
 
   return (
@@ -72,10 +81,31 @@ export function OrderHistory({
               style={{ cursor: "pointer", position: "relative" }}
             >
               <td>{order.id}</td>
-              <td>{order.store.name}</td>
+              <td>{order.store?.name}</td>
               <td>{moment(order.createdAt).format("LL")}</td>
               <td>{moment(order.createdAt).format("LT")}</td>
-              <td>{getOrderStatus(order)}</td>
+              <td className="d-md-flex">
+                <div className="w-auto h-100">
+                  <GoDotFill
+                    style={{
+                      color: orderStatusToColorMap[getOrderStatus(order)],
+                    }}
+                  />
+                </div>
+                <span className="d-none d-md-flex ps-0 ps-md-2 w-100">
+                  {getOrderStatus(order)}
+                </span>
+              </td>
+              {/* <td className="d-flex flex-column flex-md-row align-items-md-center h-100">
+                <GoDotFill
+                  style={{
+                    color: orderStatusToColorMap[getOrderStatus(order)],
+                  }}
+                />{" "}
+                <span className="d-none d-md-inline ps-0 ps-md-2 w-100">
+                  {getOrderStatus(order)}
+                </span>
+              </td> */}
               <td>{formatCurrency(order.totalPrice)}</td>
             </tr>
           ))}
