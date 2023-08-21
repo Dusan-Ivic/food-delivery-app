cd62eb6c74b02bc23b3ebb549ae3c876715390e7
Dusan-Ivic <ivicd99@gmail.com>
2023-08-21 14:27:32 +0200

    Add missing key prop for stepper's children

--- food-delivery-client/src/components/Stepper.tsx
+++ food-delivery-client/src/components/Stepper.tsx
@@ -15,7 +15,10 @@ export function Stepper({ steps, activeStep }: StepperProps) {
   return (
     <div className="d-flex justify-content-between align-items-center mb-2 bg-light p-2 rounded">
       {steps.map((step) => (
-        <div className="d-flex justify-content-center align-items-center gap-2">
+        <div
+          key={step.index}
+          className="d-flex justify-content-center align-items-center gap-2"
+        >
           <div
             className={`rounded-circle d-flex justify-content-center align-items-center text-white ${
               activeStep === step.index ? "bg-primary" : "bg-secondary"
