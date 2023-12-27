f5b24f46ebd1670f45966ce880afebe4efd06471
Dusan-Ivic <ivicd99@gmail.com>
2023-12-27 13:55:27 +0100

    Configure eslint and vite plugin checker

--- food-delivery-client/vite.config.ts
+++ food-delivery-client/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   preview: {
     host: "0.0.0.0",
     port: 3000,
