2ca907a936a9e67fcacbbc72eb542cd850affabf
Dusan-Ivic <ivicd99@gmail.com>
2024-01-02 17:12:28 +0100

    Set response interceptor for apiClient

--- food-delivery-client/src/config/apiClient.ts
+++ food-delivery-client/src/config/apiClient.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+apiClient.interceptors.response.use(null, (error) => {
+  const message = error.response?.data?.message || "Something went wrong. Please try again.";
+  toast.error(message);
+  return Promise.reject(error);
 });
 
 export default apiClient;
