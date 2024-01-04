6034242926619e8123018e2f15cd63c2ce7aca8e
Dusan-Ivic <ivicd99@gmail.com>
2024-01-04 18:07:32 +0100

    Delete remaining redux related code

--- food-delivery-client/src/app/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-  reducer: {},
-});
-
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
--- food-delivery-client/src/main.tsx
+++ food-delivery-client/src/main.tsx
@@ -4,16 +4,12 @@ import "leaflet/dist/leaflet.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import { applicationRoutes } from "@/routes/applicationRoutes";
 
 const router = createBrowserRouter(applicationRoutes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
